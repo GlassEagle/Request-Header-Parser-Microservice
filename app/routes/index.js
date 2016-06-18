7b2a79cf3db20af0a54ab7572ecd53f75dd63b87
glass eagle <mario.torres2@yahoo.com>
2016-06-18 12:30:57 +0000

    send ipaddress, language, and software as JSON

--- app/routes/index.js
+++ app/routes/index.js
@@ -6,6 +6,35 @@ module.exports = function (app) {
 
 	app.route('/')
 		.get(function (req, res) {
-			res.sendFile(path + '/public/index.html');
+			
+			var data = {
+				ipaddress: null,
+				language: null,
+				software: null
+			};
+			
+			var re;
+			
+			data.ipaddress = req.ip; //express property, depends on "trust proxy" setting
+			
+			if (req.headers) {
+				
+				if (req.headers["accept-language"]){
+					//refrence https://tools.ietf.org/html/rfc7231#section-5.3.1 to remove the qvalues
+					re = /;(?:q=)(?:0(?:.\d\d?\d?)?|1(?:.00?0?)?)/;
+					var lang = req.headers["accept-language"].replace(re, "");
+					data.language = lang;
+				}
+				
+				if(req.headers["user-agent"]){
+					//search for (os) and extract os, else just copy user agent
+					var userAgent = req.headers["user-agent"];
+					re = /\((.*)\)/;
+					var result = re.exec(userAgent);
+					data.software = result ? result[1] : userAgent;
+				}
+			}
+			
+			res.json(data);
 		});
 };
